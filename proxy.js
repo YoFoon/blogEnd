14acfbfce8255e7f876a9fa4c5b4499acf7c3ed4
YoFoon <605022360@qq.com>
2016-02-24 18:02:02 +0800

    proxy.js

--- proxy.js
+++ proxy.js
@@ -8,6 +8,9 @@ proxy.on(function(err,req,res){
 
 var server=require('http').createServer(function(req,res){
   var host= req.headers.host;
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods","GET,POST");
+  res.header("Access-Control-Allow-Headers","x-requested-with,content-type");
   switch(host){
     case 'blogend.yofoon.com':
       proxy.web(req,res,{target:'http://127.0.0.1:8000'});
