a7da71d309d7e109c488df35115c8f20a6b36074
YoFoon <605022360@qq.com>
2016-03-29 18:51:03 +0800

    上传图片

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 //var proxy = require('./proxy');
 
 var routes = require('./lib/router');
